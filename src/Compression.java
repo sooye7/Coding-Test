fbc5742efdf8e9b7222b398befaf6508b973d3cc
LEESOOYE <sooye7@naver.com>
2022-08-07 21:51:32 +0900

    LV2 압축

--- src/Compression.java
+++ src/Compression.java
@@ -1,2 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Compression {
+    public static int[] solution(String msg) {
+        int[] answer=new int[msg.length()];
+        List<String> list=new ArrayList<>();
+        // list에 알파벳 추가
+        for(int i=0;i<26;i++)
+            list.add(Character.toString('A'+i));
+        System.out.println(list);
+        for(int i=0;i<msg.length()-1;i++){
+            int index=i;
+            for(int j=i;j<index;j++);
+            if(list.contains(msg.substring(i,i+1))){
+                index++;
+                while(list.contains(msg.substring(i,index)))
+                    index++;
+                answer[i]=list.indexOf(msg.substring(i,index-1))+1;
+                list.add(msg.substring(i,index));
+            }
+        }
+        answer[msg.length()-1]=list.indexOf(msg.substring(msg.length()-1))+1;
+
+        for(int i:answer)
+            System.out.println(i);
+        return answer;
+    }
+
+    public static int[] solution2(String msg) {
+
+        List<String> list=new ArrayList<>();
+        List<Integer> ansList=new ArrayList<>();
+        // list에 알파벳 추가
+        for(int i=0;i<26;i++)
+            list.add(Character.toString('A'+i));
+        //System.out.println(list);
+        int headIdx=0, endIdx=1;
+        while(headIdx<msg.length()-1){
+            headIdx=endIdx-1;
+            if(headIdx>=msg.length()-1)
+                break;
+            while(list.contains(msg.substring(headIdx,endIdx))){
+                if(endIdx<msg.length())
+                    endIdx++;
+            }
+            ansList.add(list.indexOf(msg.substring(headIdx,endIdx-1))+1);
+            list.add(msg.substring(headIdx,endIdx));
+            System.out.println(list);
+        }
+        if(endIdx==msg.length())
+            ansList.add(list.indexOf(msg.substring(msg.length()-1))+1);
+        //System.out.println(ansList);
+        int[] answer=new int[ansList.size()];
+        for(int i=0;i<answer.length;i++)
+            answer[i]=ansList.get(i);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String msg=	"TOBEORNOTTOBEORTOBEORNOT";
+        solution2(msg);
+    }
 }
