d5c0f823cffdfb011b79ea3d79520357e24ed893
sooye7 <sooye7@naver.com>
2022-09-11 22:01:05 +0900

    LV2 땅따먹기

---
+++ src/Ttangttameokgi.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Ttangttameokgi {
+    public static int solution(int[][] land) {
+        int answer = 0;
+        int prev=0;  // 이전 열 저장
+        for(int i=0;i<land.length;i++){
+            int max=0;
+            int maxCol=0;
+            System.out.println(i+" i");
+            for(int j=0;j<land[i].length;j++){
+                System.out.println(prev);
+                if(prev==j)
+                    continue;
+                if(max<=land[i][j]){
+                    max=land[i][j];
+                    maxCol=j;
+                }
+            }
+            prev=maxCol;
+            answer+=max;
+        }
+
+        return answer;
+    }
+    public static int solution2(int[][] land) {
+        int answer = 0;
+        int prev=-1;  // 이전 열 저장
+        int max=0;
+        //int[] arr=new int[land.length*land[0].length];
+        List<Integer> list=new ArrayList<>();
+        int index=0;
+        // 1차원 배열로 변경
+        for(int[] tmp:land){
+            for(int t:tmp){
+                //arr[index++]=t;
+                list.add(t);
+            }
+        }
+        // 최댓값 찾기 (다음 요소와 이전 인덱스 차이가 4이상이면 Ok, 그렇지 않으면 값을 0으로 변경하고 건너뛰기)
+//        for(int i=0;i<land.length;i++){
+//            if(prev==-1){
+//                max=Collections.max(list);
+//                answer+=max;
+//                prev=list.indexOf(max);
+//            }
+//            else{
+//                max=Collections.max(list);
+//            }
+//        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[][] land={{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+        solution(land);
+
+    }
+}
