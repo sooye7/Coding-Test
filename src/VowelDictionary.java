92e2631d7983ba49912448d9e7adc9ec78037f0a
sooye7 <sooye7@naver.com>
2022-09-13 22:58:46 +0900

    LV2 모음사전

--- src/VowelDictionary.java
+++ src/VowelDictionary.java
@@ -1,5 +1,17 @@
 public class VowelDictionary {
-
+    public int solution(String word) {
+        int answer = word.length();
+        char[] ch = {'A', 'E', 'I', 'O', 'U'};
+        int []num = {781, 156, 31, 6, 1};
+        for(int i = 0; i < word.length(); i++){
+            for(int j = 0; j < 5; j++){
+                if(word.charAt(i) == ch[j])
+                    answer += num[i] * j;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
     public static void main(String[] args) {
 
     }
